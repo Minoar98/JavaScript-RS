c2dc1eb0e349175f964934be3e5b035f1fd292f9
Minoar98 <minoarhasan@gmail.com>
2024-10-19 21:46:06 +0600

    example-1 done

--- example-1.js
+++ example-1.js
@@ -1,7 +1,9 @@
 // 1. A function called "maxOfTwo" takes two numbers as parameters and returns the largest of the two.
 
-function maxOfTwo(){
-
+function maxOfTwo(num1, num2){
+    if(num2  > num1){
+        return num2
+    }
 }
 
-maxOfTwo(10, 20);
+console.log(maxOfTwo(10, 20));
