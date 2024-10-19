971aad2078eb3989c8b587b7742ac60c26480b03
Minoar98 <minoarhasan@gmail.com>
2024-10-19 23:44:46 +0600

    example-3 done

---
+++ example-3.js
@@ -0,0 +1,6 @@
+//3. A function called "convertToFahrenheit" takes a temperature in Celsius as a parameter and returns the temperature in Fahrenheit.
+ function convertToFahrenheit(Celsius){
+    return (Celsius * 9/5) + 32
+ }
+
+ console.log(convertToFahrenheit(80));
