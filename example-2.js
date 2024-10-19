4c55b467666df284c798437039c2c0e19a4ca8f4
Minoar98 <minoarhasan@gmail.com>
2024-10-19 21:52:55 +0600

    example-2 done

---
+++ example-2.js
@@ -0,0 +1,10 @@
+//2. A function called "minOfTwo" takes two numbers as parameters and returns the smallest of the two.
+
+function minOfTwo(num1, num2){
+    if(num1 < num2){
+        return num1
+
+    }
+
+}
+console.log(minOfTwo(30, 45));
