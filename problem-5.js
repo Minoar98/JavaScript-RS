503b08197ae23890ba5ea982220fe6fd85d1ddc8
Minoar98 <minoarhasan@gmail.com>
2024-10-20 16:29:36 +0600

    problem-5 added

---
+++ problem-5.js
@@ -0,0 +1,7 @@
+//5. A function called "division" takes in two parameters and returns the division of the two numbers.
+
+function division(num1, num2){
+   return num1 / num2;
+
+}
+console.log(division(125, 25));
