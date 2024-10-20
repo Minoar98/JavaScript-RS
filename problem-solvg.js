2ae53137e82f9c22647559af2d213ac6ebe7c58f
Minoar98 <minoarhasan@gmail.com>
2024-10-20 11:04:20 +0600

    if else added

---
+++ problem-solvg.js
@@ -0,0 +1,22 @@
+// const time = new Date().getHours();
+// let greeting;
+// if (time < 30) {
+//   greeting = "Good morning";
+// } else if (time < 20) {
+//   greeting = "Good day";
+// } else {
+//   greeting = "Good evening";
+// }
+// console.log(greeting)
+
+let age = 50;
+
+if(age < 20){
+    console.log("You are young");
+} else if(age == 18){
+    console.log("You are adult");
+} else if(age > 18 && age < 30){
+    console.log("You are matuerd");
+} else {
+    console.log("You are old")
+}
