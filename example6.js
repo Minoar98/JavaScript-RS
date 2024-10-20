3dfe991a1f5ddd0d549dbb982c270614c748cbf4
Minoar98 <minoarhasan@gmail.com>
2024-10-20 18:50:12 +0600

    problem-6 addded

---
+++ example6.js
@@ -0,0 +1,6 @@
+//A function called "division" takes a parameter and returns the division by 5. (use default parameter).
+
+function division(num = 25){
+    return num / 5
+}
+console.log(division(30));
