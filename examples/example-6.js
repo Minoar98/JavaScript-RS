f708ba4d2f95d2f3dadd75e222cba4ceb6daed0c
Minoar98 <minoarhasan@gmail.com>
2024-10-22 01:20:16 +0600

    converted arrow fun

--- examples/example-6.js
+++ examples/example-6.js
@@ -4,3 +4,14 @@ function division(num = 25){
     return num / 5
 }
 console.log(division(30));
+console.log(division());
+
+//defaul parameter added
+
+//arrow function
+
+const division1 = (num = 25) => {
+    return num / 5
+}
+console.log(division1(180));
+console.log(division1())
--- examples/home-prac.js
+++ examples/home-prac.js
@@ -30,3 +30,82 @@ const myFunc6 = (val1, val2) => console.log(val1 + val2);
 const myFunc7 =(val1, x = 10) => console.log(val1 + x);
 const myFunc8 =(val1) => val1
 
+// 2 parameter no return
+function barcPlayer (firstName, lastName){
+    console.log(`${firstName} ${lastName}`)
+  }
+  barcPlayer("Lamal","Yamel");
+
+// 2 parameter no return
+const barcPlayer1 = (firstName, lastName) =>{
+    console.log(`${firstName} ${lastName}`)
+  }
+  barcPlayer1("Lamal","Yamel");
+
+  //A function takes a negative number & returns after converting same number as a positive number.
+
+function myNumber(num){
+    return -(num);
+}
+console.log(myNumber(-10));
+
+//converted arrow
+
+const myNumber1 = (num) =>{
+    return -(num)
+}
+console.log(myNumber1(-20));
+
+// A function called “greetings” takes a name & returns ‘Greeting x’.
+
+function greetings(name = "Sunfrancisco"){
+    return `Greeting ${name}`
+}
+console.log (greetings("Chicago"));
+console.log(greetings("Newyork"))
+
+////1. A function that takes km as input & returns in miles.convert arrow function
+
+const convertKm = (num) =>{
+    return num * 0.621371
+}
+console.log(convertKm(220));
+
+////A function takes a number & returns a boolean if a number is divisible by 10
+
+function myNum (num, x = 10){
+    if (num % x === 0) {
+        return true;
+    } else{
+        return false;
+    }
+}
+
+console.log(myNum(10));
+console.log(myNum(15));
+
+// alternative
+
+function myNum1(num, x = 10){
+    return (num % x === 0)
+}
+console.log(myNum1(10));
+console.log(myNum1(15));
+
+//arrow function as convert as
+const myNum2 = (num, x = 10) =>{
+    if(num % x === 0){
+        return true
+    } else {
+        return false
+    }
+}
+console.log(myNum2(10));
+console.log(myNum2(15));
+
+// alternative 
+const myNum3 =(num, x= 10) => {
+    return (num % x === 0)
+}
+console.log(myNum3(10));
+console.log(myNum3(15));
