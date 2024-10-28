bcf0fe45172d8123349f2aea5eb4764ee9cc06fa
Minoar98 <minoarhasan@gmail.com>
2024-10-28 12:00:18 +0600

    task-4 added

--- examples/task-2.js
+++ examples/task-2.js
@@ -11,6 +11,6 @@ let i = 1;
 while(i <= 10){
     if(i % 2 ===0){
         console.log(i);
-        i++
     }
+    i++
 }
---
+++ examples/task-4.js
@@ -0,0 +1,16 @@
+//print all the  numbers form 1-20 using for loop that is divisble by 3
+
+for (let i = 1; i <= 20; i++){
+    if(i % 3 === 0){
+        console.log(i);
+    }
+}
+
+////print all the  numbers form 1-20 using while loop that is divisble by 3
+let i = 1;
+while(i <= 20){
+    if(i % 3 === 0){
+     console.log(i);   
+    }
+    i++
+}
