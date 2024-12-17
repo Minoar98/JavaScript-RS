729b941a1eb6c5ae0b806cdb830e5134a4534d56
Minoar98 <minoarhasan@gmail.com>
2024-12-17 18:10:30 +0600

    arr-method added

--- Array & loop example/array.obj-22.js
+++ Array & loop example/array.obj-22.js
@@ -1,3 +1,22 @@
 //  [-20, 50, -34, 55, 95] is an array. Find the difference between maximum & minimum number.
 
+function findDiffBetwnnMaxMin(arr) {
+  let max = -Infinity;
+  let min = Infinity
+  const maxValue = arr.forEach((value) => {
+    if (value > max) {
+      max = value;
+    }
+    if (value < min) {
+      min = value;
+    }
+  });
+
+  return max - min;
+}
+
+const arr = [-20, 50, -34, 55, 95]; 
+const result = findDiffBetwnnMaxMin(arr);
+console.log(result);
+
 
--- arr,loop/sample-question-15.js
+++ arr,loop/sample-question-15.js
@@ -1,15 +1,15 @@
 //[-10, 20, -44, 35, 65, 40, 22, -33, 51, -39] is an array. Print the minimum number of odd indexed numbers.
 
 const findMinOddIndexNumber = () => {
-    let min = 1000;
-    for(let i = 1; i < array.length; i+= 2) {
-        if(array[i] < min) {
-            // console.log(array[i] + ' ' + i);
-            min = array [i];
-        }
+  let min = 1000;
+  for (let i = 1; i < array.length; i += 2) {
+    if (array[i] < min) {
+      // console.log(array[i] + ' ' + i);
+      min = array[i];
     }
-    console.log("The minimum number of odd-indexed is:", min);
-}
+  }
+  console.log("The minimum number of odd-indexed is:", min);
+};
 
-const array = [-10, 20, -44, 35, 65, 40, 22, -33, 51, -39]
-findMinEvenIndexNumber(array)
+const array = [-10, 20, -44, 35, 65, 40, 22, -33, 51, -39];
+findMinOddIndexNumber(array);
