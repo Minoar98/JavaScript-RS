2fbef6506360bec527cc29f9c13eb964017f72c5
Minoar98 <minoarhasan@gmail.com>
2025-01-11 00:47:27 +0600

    arr-method added

---
+++ Array-string-method/array-string-1.js
@@ -0,0 +1,13 @@
+// 1. ["hello", "world", "javascript", "is", "fun"] is an array. Create a new array like the output
+// Output:
+// [ { word: 'hello', length: 5 }, { word: 'world', length: 5 }, { word: 'javascript', length: 10 }, { word: 'is', length: 2 }, { word: 'fun', length: 3 } ]
+
+const arr = ["hello", "world", "javascript", "is", "fun"];
+console.log(
+  arr.map((word) => {
+    return {
+      word: word,
+      length: word.length,
+    };
+  })
+);
