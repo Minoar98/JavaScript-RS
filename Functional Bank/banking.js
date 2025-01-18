e338004a43da3a51d86d7c5a351fc5dcf50d840c
Minoar98 <minoarhasan@gmail.com>
2025-01-18 21:14:48 +0600

    create function addded

--- Functional Bank/banking.js
+++ Functional Bank/banking.js
@@ -1,28 +1,27 @@
 // Create Account
 
-function createAccount(account) {
-  const users = [
-    {
-      id: Date.now().toString(),
-      name: "SYED MINAOR HASN",
-      bankInfo: {
-        accountNo: "1740850027450",
-        balance: "$1000",
-      },
+const users = [
+  {
+    id: Date.now().toString(),
+    name: "SYED MINAOR HASN",
+    bankInfo: {
+      accountNo: "1740850027450",
+      balance: "$1000",
     },
-    // {
-    //   id: Date.now().toString(),
-    //   name: "Rezoan Shakil Prince",
-    //   bankInfo: {
-    //     accountNo: "1711050007520",
-    //     balance: "$2000000000000",
-    //   },
-    // },
-  ];
+  },
+  // {
+  //   id: Date.now().toString(),
+  //   name: "Rezoan Shakil Prince",
+  //   bankInfo: {
+  //     accountNo: "1711050007520",
+  //     balance: "$2000000000000",
+  //   },
+  // },
+];
 
-  console.log("A new user is created.");
-  console.log(users);
-  return account;
+function CreateAccount() {
+  console.log("A new user is created");
 }
-// Now call the function
-createAccount(account);
+
+CreateAccount();
+console.log(users);
