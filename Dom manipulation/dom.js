bd325a850ef93df1a3e776150c8dc4e288aabb5c
Minoar98 <minoarhasan@gmail.com>
2025-02-25 23:32:28 +0600

    dom-added

---
+++ Dom manipulation/dom.js
@@ -0,0 +1,24 @@
+const h1 = document.getElementsByTagName("h1");
+console.log(h1);
+console.log(h1[1].innerHTML);
+
+const p1 = document.getElementById("pip");
+console.log(p1.innerHTML);
+const className = document.getElementsByClassName("Lorm");
+console.log(className[0].innerHTML);
+
+const he = document.getElementsByClassName("Lorm");
+console.log(he);
+
+const p11 = document.querySelector("#pip");
+console.log(p11.innerHTML);
+
+const className1 = document.querySelector(".Lorm");
+
+console.log(className1);
+
+const he1 = document.querySelector("h1");
+console.log(he1);
+
+const h22 = document.querySelectorAll(".hur");
+console.log(h22);
---
+++ domjs/dom-prac.js
@@ -0,0 +1,13 @@
+const pId = document.getElementById("id");
+const pClass = document.getElementsByClassName("class1");
+const p = document.getElementsByTagName("p");
+
+// css selector
+document.querySelector("p");
+document.querySelector("#id");
+document.querySelector("class-1");
+
+// return NodeList (Array Like)
+
+document.querySelectorAll("p");
+document.querySelectorAll("class-1");
