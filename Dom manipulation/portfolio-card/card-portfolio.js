e31a819c2d665ca1e50c611eb1916cfbaf61e31c
Minoar98 <minoarhasan@gmail.com>
2025-03-06 01:46:41 +0600

    domm added

---
+++ Dom manipulation/portfolio-card/card-portfolio.js
@@ -0,0 +1,6 @@
+// create newelement
+const newElement = document.createElement("h4");
+newElement.innerHTML = "Frontend";
+console.log(newElement);
+console.dir(newElement);
+// document.appendChild("newElement");
