8864e60e1b3c5dc984fc0de34b277327da7a927f
Minoar98 <minoarhasan@gmail.com>
2025-03-15 09:36:06 +0600

    fun-bac update

--- exam-app/nid.js
+++ exam-app/nid.js
@@ -8,7 +8,75 @@
 // DOB: 18-02-1789
 // NID No: 123-456-789
 
-// 1. showNIDInfo()
+const showNIDInfo = (nidInfo, nidNo, isThirdParty = false) => {
+  if (!isThirdParty) {
+    console.log("User information:");
+    console.log(`Name: ${nidInfo.name}`);
+    console.log(`Father Name: ${nidInfo.fatherName}`);
+    console.log(`Mother Name: ${nidInfo.motherName}`);
+    console.log(`DOB: ${nidInfo.dob}`);
+    console.log(`NID No: ${nidInfo.nidNo}`);
+  } else {
+    // user is found
+    if (nidInfo.nidNo === nidNo) {
+      console.log("User information:");
+      console.log(`Name: ${nidInfo.name}`);
+      console.log(`DOB: ${nidInfo.dob}`);
+      console.log(`NID No: ${nidInfo.nidNo}`);
+    } else {
+      // user is not found
+      console.log("The person you have searched is not found");
+      console.log("Please try again later");
+    }
+  }
+};
+
+const createNID = (info) => {
+  // create nid for an user
+  const userNID = {
+    name: info.name,
+    fatherName: info.fatherName,
+    motherName: info.motherName,
+    dob: info.dob,
+    nidNo: info.nidNo,
+  };
+
+  console.log("A new NID for an user is created.");
+  showNIDInfo(userNID, info.nidNo);
+
+  return userNID;
+};
+
+const info = {
+  name: "Mizanur Rahman",
+  fatherName: "Fazla Rabbi",
+  motherName: "Shimla Rahmann",
+  dob: "18-02-1789",
+  nidNo: "123-456-789",
+};
+
+// main object
+let userNID = {};
+console.log("===============Create NID=======================");
+userNID = createNID(info);
+
+console.log("===============Show NID For Myself=======================");
+showNIDInfo(userNID, info.nidNo);
+
+console.log("===============Show NID For Third Party=======================");
+showNIDInfo(userNID, info.nidNo, true);
+
+console.log("===============Show NID For Third Party=======================");
+showNIDInfo(userNID, "123-456-783", true);
+
+// 2. showNIDInfo()
+
+// User information:
+// Name: Mizanur Rahman
+// Father Name: Fazla Rabbi
+// Mother Name: Shimla Rahmann
+// DOB: 18-02-1789
+// NID No: 123-456-789
 
 // 3. showNIDInfo() // Any third parties like bank/passport office etc
 
@@ -53,34 +121,36 @@
 // - Reuse already written function - 3 marks
 
 // Overall presentation - 7 marks
-const showNIDInfo = (userInfo) => {
-  console.log("User information:");
-};
 
-//*-------------------------------start- createNID-----------------------------------****//
-const createNID = (name, fatherName, motherName, dob, nidNo) => {
-  user = {
-    Name: name,
-    FatherName: fatherName,
-    MotherName: motherName,
-    DOB: dob,
-    NIDNo: nidNo,
-  };
+// Overall presentation - 7 marks
+// const showNIDInfo = (userInfo) => {
+//   console.log("User information:");
+// };
 
-  console.log("A new NID for an user is created.");
+// //*-------------------------------start- createNID-----------------------------------****//
+// const createNID = (name, fatherName, motherName, dob, nidNo) => {
+//   user = {
+//     Name: name,
+//     FatherName: fatherName,
+//     MotherName: motherName,
+//     DOB: dob,
+//     NIDNo: nidNo,
+//   };
 
-  //   return showNIDInfo(user);
-};
+//   console.log("A new NID for an user is created.");
 
-//*-------------------------------start- createNID-----------------------------------****//
+//   return showNIDInfo(user);
+// };
 
-let user = null;
-createNID(
-  "Mizanur Rahman",
-  "Fazla Rabbi",
-  "Shimla Rahmann",
-  "18-02-1789",
-  "123-456-789"
-);
+// //*-------------------------------start- createNID-----------------------------------****//
 
-console.log(user);
+// let user = null;
+// createNID(
+//   "Mizanur Rahman",
+//   "Fazla Rabbi",
+//   "Shimla Rahmann",
+//   "18-02-1789",
+//   "123-456-789"
+// );
+
+// console.log(user);
